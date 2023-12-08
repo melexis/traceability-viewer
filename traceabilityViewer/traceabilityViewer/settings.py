1e830e0d8cf39bd4c76957ed4d6fe4c21986698f
JokeWaumans <jwm@melexis.com>
2023-12-08 11:30:48 +0100

    Update settings

--- traceabilityViewer/traceabilityViewer/settings.py
+++ traceabilityViewer/traceabilityViewer/settings.py
@@ -91,12 +91,6 @@ __VUE_PROD_DEVTOOLS__ = True
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
