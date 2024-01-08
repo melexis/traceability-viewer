5d65dc195eefc47517b2f25d6919851d9ce0fdcd
JokeWaumans <jwm@melexis.com>
2024-01-08 14:09:19 +0100

    Add alert components

---
+++ traceabilityViewer/myapp/static/components/alert.js
@@ -0,0 +1,54 @@
+app.component("alert", {
+    delimiters: ["[[", "]]"],
+    template:
+        /*html*/
+        `
+        <div ref="alert" :id="'alert-' + alertData.number" class="alert alert-danger alert-dismissible fade show" role="alert">
+            <div class="d-flex">
+                <svg width="25" height="25" class="bi flex-shrink-1 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                <button style="--bs-btn-padding-y: 0; --bs-btn-padding-x: .5rem;" data-bs-toggle="collapse" :data-bs-target="'#error-' + alertData.number" aria-expanded="false" :aria-controls="'error-' + alertData.number" type="button" class="me-2 btn btn-outline-secondary" @click="show = !show">[[buttonText]]</button>
+                <strong>Error: &nbsp;</strong><div ref="title">[[alertData.title]] [[show]]</div>&emsp;
+                <button type="button" class="btn-close" :data-bs-dismiss="'alert-' + alertData.number" aria-label="Close" @click="closeAlert"></button>
+            </div>
+            <div :id="'error-' + alertData.number" class="collapse" >
+                <div class="font-monospace mt-2" style="white-space: pre-wrap">
+                [[alertData.message]]
+                </div>
+            </div>
+        </div>
+      `,
+    props: {
+      alertData: {
+        type: Object,
+      },
+    },
+    emits: ["closeAlert"],
+    setup(props, { emit }) {
+        let show = Vue.ref(false);
+        let alert = Vue.ref(null);
+        let alertData = Vue.toRef(props, "alertData");
+
+        let buttonText = Vue.computed(() =>  {
+            if (show.value){
+                return "Hide error message"
+            }
+            return "Show error message"
+        })
+
+        function closeAlert() {
+            emit("closeAlert", alertData.value.number);
+        }
+
+        Vue.onMounted(function () {
+            alert.value.scrollIntoView({behavior: "smooth"})
+          });
+
+        return {
+            show,
+            alert,
+            alertData,
+            buttonText,
+            closeAlert,
+        }
+    }
+})
--- traceabilityViewer/myapp/static/components/traceabilityViewer.js
+++ traceabilityViewer/myapp/static/components/traceabilityViewer.js
@@ -32,20 +32,12 @@ app.component("traceability-viewer", {
             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </symbol>
     </svg>
-
-    <div ref="alert" id="alert" class="alert alert-danger alert-dismissible fade show" role="alert">
-      <div class="d-flex">
-          <svg width="20" height="20" class="bi flex-shrink-1 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-          <button id="arrow-button" data-bs-toggle="collapse" data-bs-target="#error" aria-expanded="false" aria-controls="error" type="button" class="btn" content-type="charset=utf-8">&#8964</button>
-          <strong>Error:&emsp;</strong><div ref="info"></div>&emsp;
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-      <div id="error" class="collapse" >
-        <div class="font-monospace" style="white-space: pre-wrap">
-        [[errorText]]
-        </div>
-      </div>
-    </div>
+    <alert
+      v-for="alert in alerts"
+      :alertData="alert"
+      @closeAlert="closeAlert"
+    >
+    </alert>
     <!-- Navbar -->
     <div class="d-flex flex-wrap">
         <button type="button" @click="clicked" class="btn"
@@ -89,11 +81,6 @@ app.component("traceability-viewer", {
     <graphviz :loading="loading" :nodes="nodes" :links="links" :config="config"> </graphviz>
     `,
   setup() {
-    var show = false;
-    var errorText = Vue.ref("");
-    var error = Vue.ref(null);
-    var alert = Vue.ref(null);
-    var info = Vue.ref(null);
     var loading = Vue.ref(true);
     var query = Vue.ref("");
     var activeGroup = Vue.ref("home");
@@ -106,6 +93,8 @@ app.component("traceability-viewer", {
     var links = Vue.shallowRef([]);
     var initNodes = [];
     var initLinks = [];
+    var alerts = Vue.ref([]);
+    var alertNumber = 0;
 
     function clicked() {
       activeGroup.value = "home";
@@ -124,21 +113,20 @@ app.component("traceability-viewer", {
       links.value = data.links;
     }
 
-    function onAlert(data) {
-      console.log(data)
-      errorText.value = data.message;
-      info.value.innerText = data.title;
-      alert.value.scrollIntoView({behavior: "smooth"})
-      // alert.value.style.display = "block";
+    function onAlert(alertData) {
+      alertData["number"] = alertNumber;
+      alerts.value.push(alertData);
+      alertNumber += 1;
     }
 
-    // function close() {
-    //   alert.value.style.display = "none";
-    //   error.value.className = "{show: false}"
-    //   document.getElementById("arrow-button").innerHTML = "&#8964";
-    //   info.value.innerText = "";
-    //   show = false;
-    // }
+    function closeAlert(number){
+      alerts.value = alerts.value.filter((element) => {
+        if (element.number == number){
+          return false
+        }
+        return true
+      })
+    }
 
     function changeLoading(newValue) {
       loading.value = newValue;
@@ -171,10 +159,7 @@ app.component("traceability-viewer", {
     });
 
     return {
-      error,
-      alert,
-      errorText,
-      info,
+      alerts,
       loading,
       query,
       activeGroup,
@@ -189,6 +174,7 @@ app.component("traceability-viewer", {
       changeGroup,
       changeData,
       onAlert,
+      closeAlert,
       changeLoading,
       initialize,
     };
