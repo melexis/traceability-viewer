f5b265b62c4b8e08cd0ff66eb0bd325d1e1ad19b
JokeWaumans <jwm@melexis.com>
2024-01-12 12:11:54 +0100

    Use identifier in alert component

--- traceabilityViewer/myapp/static/components/alert.js
+++ traceabilityViewer/myapp/static/components/alert.js
@@ -3,14 +3,14 @@ app.component("alert", {
     template:
         /*html*/
         `
-        <div ref="alert" :key="alertData.index" :id="'alert-' + alertData.index" class="alert alert-danger alert-dismissible fade show" role="alert">
+        <div ref="alert" :id="'alert-' + alertData.identifier" class="alert alert-danger alert-dismissible fade show" role="alert">
             <div class="d-flex">
                 <svg width="25" height="25" class="bi flex-shrink-1 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-                <button style="--bs-btn-padding-y: 0; --bs-btn-padding-x: .5rem;" data-bs-toggle="collapse" :data-bs-target="'#error-' + alertData.index" aria-expanded="false" :aria-controls="'error-' + alertData.index" type="button" class="me-2 btn btn-outline-secondary" @click="show = !show">[[buttonText]]</button>
+                <button v-if="alertData.message != ''" style="--bs-btn-padding-y: 0; --bs-btn-padding-x: .5rem;" data-bs-toggle="collapse" :data-bs-target="'#error-' + alertData.identifier" aria-expanded="false" :aria-controls="'error-' + alertData.index" type="button" class="me-2 btn btn-outline-secondary" @click="show = !show">[[buttonText]]</button>
                 <strong>Error: &nbsp;</strong><div ref="title">[[alertData.title]]</div>&emsp;
-                <button type="button" class="btn-close" :data-bs-dismiss="'alert-' + alertData.index" aria-label="Close" @click="removeAlert"></button>
+                <button type="button" class="btn-close" :data-bs-dismiss="'alert-' + alertData.identifier" aria-label="Close" @click="removeAlert"></button>
             </div>
-            <div :id="'error-' + alertData.index" class="collapse" >
+            <div :id="'error-' + alertData.identifier" class="collapse" >
                 <div class="font-monospace mt-2" style="white-space: pre-wrap">
                 [[alertData.message]]
                 </div>
@@ -35,7 +35,7 @@ app.component("alert", {
         })
 
         function removeAlert() {
-            emit("removeAlert", props.alertData.index);
+            emit("removeAlert", props.alertData.identifier);
         }
 
         Vue.onMounted(function () {
