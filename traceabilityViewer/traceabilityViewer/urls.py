115091ee0394f91486433e4992b422aa82b66f5a
Thanos Mitsios <atm@melexis.com>
2024-02-15 18:04:17 +0200

    test

--- traceabilityViewer/traceabilityViewer/urls.py
+++ traceabilityViewer/traceabilityViewer/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("app.urls")),
     path("favicon.ico", RedirectView.as_view(url="app/static/images/favicon.ico")),
 ]
+urlpatterns += staticfiles_urlpatterns()
