8d6e5d4566ca65f4d6965480bd60c60ce5ce4a07
JokeWaumans <jwm@melexis.com>
2024-02-15 13:48:56 +0100

    Set `DEBUG` to `False`

--- traceabilityViewer/traceabilityViewer/settings.py
+++ traceabilityViewer/traceabilityViewer/settings.py
@@ -27,7 +27,7 @@ config.encoding = "cp1251"
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
